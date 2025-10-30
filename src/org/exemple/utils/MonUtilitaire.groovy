540c4ba0cde1b5627067c30c8ccaee3edb7eaad4
yanis-montgenie <166261724+yanis-montgenie@users.noreply.github.com>
2025-10-30 15:12:26 +0100

    Add greeting message to saluer method

--- src/org/exemple/utils/MonUtilitaire.groovy
+++ src/org/exemple/utils/MonUtilitaire.groovy
@@ -4,6 +4,7 @@ package org.exemple.utils
 class MonUtilitaire implements Serializable {
     static void saluer(script, nom) {
         script.echo "Bonjour de la Shared Library, ${nom} !"
+        script.echo "Modification partie 5 de , ${nom} !"
     }
 
     static String genererMessage(String type) {
